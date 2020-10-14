9fdd51ff88e52319c31f3e816e0e74a2df90fb60
Joanna-Joy Gray <jjgray21@hotmail.com>
2020-10-14 00:07:46 -0500

    Added X on click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,13 +2,30 @@ window.onload = function(e){
     let boxes = document.querySelectorAll('#board > div');
     console.log("test");
     boxes.forEach(function(elem, index, list){
-        console.log("check");
         //elem.addEventListener('onload', function(e){
          //   e.target.classList.add('square');
         //});
         elem.classList.add('square');
     });
+
+    let boxes2 = document.querySelectorAll('#board > div');
+    boxes2.forEach(function(elem, index, list){
+        elem.addEventListener('click', function(e){
+            console.log("check");
+            console.log(e.target);
+            //e.target.classList.remove('square');
+            e.target.classList.add('square.X');
+            e.target.innerHTML = "X";
+    });
+    });
+
+    /*let boxes = document.querySelectorAll('#board > div');
+    elem.addEventListener('click', function(e){
+       e.target.classList.add('square.X');*/
 };
+
+
+
 
 
 
